3b94f7bf764b3a9175e2d2acb7469aa31810ab62
Javier <alexerazoajebsk8@outlook.com>
2021-01-15 00:02:24 -0500

    mide distancia de un celular, Avance 1

--- deteccion_video.py
+++ deteccion_video.py
@@ -10,6 +10,10 @@ from PIL import Image
 import torch
 from torch.autograd import Variable
 
+##Datos iniciales para medir distancia de un celular a la camara (CM)
+DISTANCIA_INIC_CELULAR = 50 
+PIXELES_DIST_INIC_CELULAR = 380 #Tomado del video
+ANCHO_REAL_CELULAR = 13.5
 
 
 def Convertir_RGB(img):
@@ -33,7 +37,26 @@ def Convertir_BGR(img):
     img[:, :, 2] = r
     return img
 
+def dibujarLinea(imagen, puntoInicial, puntoFinal, color, grosor):
+    cv2.line(imagen, puntoInicial, puntoFinal, color, grosor)
 
+def obtenerCentroide(x1, y1, x2, y2):
+    return ((x1 + x2)/2, (y1 + y2) / 2 )
+
+def obtenerAnchoCaja(x1, x2):
+    return round(float(x2) - float(x1), 2)
+
+def calcularFocal(anchoPixeles, distInicial, anchoReal):
+    return (anchoPixeles*distInicial)/anchoReal
+
+
+#def calcularDistancia(anchoPixeles, distInicial, anchoReal, anchoVariable):
+   #la distancia se calcula para un objeto como un celular
+#    focal = calcularFocal(anchoPixeles, distInicial, anchoReal)
+#    return anchoReal * focal / anchoVariable
+
+def calcularDistancia(anchoVariable, distInic, pixInic):
+    return round(pixInic*distInic/anchoVariable, 2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -51,6 +74,7 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
     opt = parser.parse_args()
     print(opt)
+    #Si encuentra una GPU lo usa sino usa CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("cuda" if torch.cuda.is_available() else "cpu")
     model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
@@ -65,21 +89,26 @@ if __name__ == "__main__":
     classes = load_classes(opt.class_path)
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
     if opt.webcam==1:
+        #Aqui toma el video de la webcam
         cap = cv2.VideoCapture(0)
         out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280,960))
     else:
+        #Toma el video que este en el directorio
         cap = cv2.VideoCapture(opt.directorio_video)
-        # frame_width = int(cap.get(3))
-        # frame_height = int(cap.get(4))
         out = cv2.VideoWriter('outp.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280,960))
     colors = np.random.randint(0, 255, size=(len(classes), 3), dtype="uint8")
     a=[]
+
+    #Comienza a procesar las imagenes del video
     while cap:
+        #Abrimos la primera imagen del video
         ret, frame = cap.read()
         if ret is False:
             break
+        #Creamos e; frame 
         frame = cv2.resize(frame, (1280, 960), interpolation=cv2.INTER_CUBIC)
         #LA imagen viene en Blue, Green, Red y la convertimos a RGB que es la entrada que requiere el modelo
+        #Preparamos la imagen de aucerdo a los tensores para que el modelo pueda entenderlo
         RGBimg=Convertir_RGB(frame)
         imgTensor = transforms.ToTensor()(RGBimg)
         imgTensor, _ = pad_to_square(imgTensor, 0)
@@ -89,21 +118,34 @@ if __name__ == "__main__":
 
 
         with torch.no_grad():
+            #Le pasamos el tensor con la imagen preparada al modelo
             detections = model(imgTensor)
             detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
 
-
+        #las detecciones nos van a dar el numero de la etiqueta (id)
+        #Dibujamos la caja, obtenemos coordenadas, certeza y nombre de la clase 
         for detection in detections:
             if detection is not None:
                 detection = rescale_boxes(detection, opt.img_size, RGBimg.shape[:2])
                 for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
-                    box_w = x2 - x1
-                    box_h = y2 - y1
+                    #Calculamos dimensiones de la caja
+                    ancho_caja = x2 - x1
+                    altura_caja = y2 - y1
                     color = [int(c) for c in colors[int(cls_pred)]]
                     print("Se detectó {} en X1: {}, Y1: {}, X2: {}, Y2: {}".format(classes[int(cls_pred)], x1, y1, x2, y2))
-                    frame = cv2.rectangle(frame, (x1, y1 + box_h), (x2, y1), color, 5)
+                    frame = cv2.rectangle(frame, (x1, y1 + altura_caja), (x2, y1), color, 5)
                     cv2.putText(frame, classes[int(cls_pred)], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 5)# Nombre de la clase detectada
-                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - box_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 5) # Certeza de prediccion de la clase
+                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - altura_caja), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 5) # Certeza de prediccion de la clase
+                    #Dibujamos la linea en el centro de cada imagen
+                    centroide = obtenerCentroide(x1, y1, x2, y2)
+                    dibujarLinea(frame, (640,960), centroide, (0,255,0), 3)
+                    
+                    #CALCULAMOS LA DISTANCIA
+                    ancho_caja_trans = obtenerAnchoCaja(x1, x2)
+                    distancia = calcularDistancia(ancho_caja_trans, DISTANCIA_INIC_CELULAR, PIXELES_DIST_INIC_CELULAR)
+                    #Escribimos la distancia
+                    cv2.putText(frame, str(distancia) + "cm", centroide, 0, 1, (255,0,0), 3, cv2.LINE_AA)
+                
         #
         #Convertimos de vuelta a BGR para que cv2 pueda desplegarlo en los colores correctos
         
